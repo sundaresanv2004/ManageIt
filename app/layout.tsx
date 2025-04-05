3d54ee69ba9f6a94ee2e73d5594ca275bf8ec260
Sundaresan V <sundaresanv2004@gmail.com>
2025-04-05 10:15:31 +0530

    Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import {ThemeProvider} from "next-themes";
+import {ThemeProvider} from "@/components/ui/theme-provider";
 import {Toaster} from "@/components/ui/toaster";
 import AuthProvider from "@/app/auth/Provider";
 
